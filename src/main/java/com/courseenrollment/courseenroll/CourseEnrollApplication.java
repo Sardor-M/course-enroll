dd87155d805aeda47360826cd698ebf990bd810a
Sardor Madaminov <sardor0968@gmail.com>
2023-04-11 22:20:10 +0900

    Initial commit [project]

---
+++ src/main/java/com/courseenrollment/courseenroll/CourseEnrollApplication.java
@@ -0,0 +1,13 @@
+package com.courseenrollment.courseenroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEnrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEnrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/courseenrollment/courseenroll/CourseEnrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courseenrollment.courseenroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEnrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

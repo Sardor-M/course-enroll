9189074f9f53936afff79006f4de6a11e55a5850
Sardor Madaminov <sardor0968@gmail.com>
2023-04-13 23:50:43 +0900

    minor changes: added

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./components/views/MainPage/MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<MainPage />} />
+      </Routes>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
